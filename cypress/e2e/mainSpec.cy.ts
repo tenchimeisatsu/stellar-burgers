9b83f9b1769e94702cb79c8fda273bc874d06ac8
Artyom Kondratev <kondratyev.art@yandex.ru>
2025-05-06 01:13:23 +0300

    Sprint 12 Second Review

--- cypress/e2e/mainSpec.cy.ts
+++ cypress/e2e/mainSpec.cy.ts
@@ -1,3 +1,22 @@
+Cypress.Commands.addAll({
+  selectBun() {
+    cy.get('[data-cy^=bun] button').first().click();
+  },
+  selectMain() {
+    cy.get('[data-cy^=main] button').first().click();
+  },
+  selectSauce() {
+    cy.get('[data-cy^=sauce] button').first().click();
+  },
+  closeModal() {
+    cy.get('[data-cy=modal] button').click();
+  },
+  isClearConstructor() {
+    cy.get('[data-cy=noUpperBun]').should('have.text', 'Выберите булки');
+    cy.get('[data-cy=noIngredients]').should('have.text', 'Выберите начинку');
+    cy.get('[data-cy=noLowerBun]').should('have.text', 'Выберите булки');
+  }
+});
 beforeEach(() => {
   cy.intercept('api/ingredients', { fixture: 'mockIngredients.json' }).as(
     'getIngredients'
@@ -8,30 +27,36 @@ beforeEach(() => {
       window.localStorage.setItem('refreshToken', 'refreshToken');
     })
     .as('getUser');
-  cy.visit('http://localhost:4000');
+  cy.visit('/');
+});
+afterEach(() => {
+  cy.clearCookie('accessToken');
+  window.localStorage.removeItem('refreshToken');
 });
 describe('Burger constructor test', () => {
   it('should add ingredients correctly', () => {
-    cy.get('[data-cy^=bun] button').first().click();
-    cy.get('[data-cy^=main] button').first().click();
-    cy.get('[data-cy^=sauce] button').first().click();
+    cy.isClearConstructor();
+    cy.selectBun();
+    cy.selectMain();
+    cy.selectSauce();
     cy.get('[data-cy=upperBun]').should('contain.text', 'булка');
     cy.get('[data-cy^=ingredient]').should('have.length', '2');
   });
   it('should open and close modal', () => {
-    cy.get('[data-cy^=bun]').first().click();
-    cy.get('[data-cy=modal]').should('contain.text', 'Детали');
-    cy.get('[data-cy=modal] button').click();
     cy.get('[data-cy=modal]').should('not.exist');
+    cy.get('[data-cy^=bun]').first().click();
+    cy.get('[data-cy=modal]').as('modal');
+    cy.get('@modal').should('contain.text', 'Детали');
+    cy.closeModal();
+    cy.get('@modal').should('not.exist');
     //check close by overlay
     cy.get('[data-cy^=bun]:first').click();
     cy.get('[data-cy=outsideModal]').click({ force: true });
-    cy.get('[data-cy=modal]').should('not.exist');
+    cy.get('@modal').should('not.exist');
   });
   it('should correctly create order', () => {
-    cy.get('[data-cy^=bun] button').first().click();
-    cy.get('[data-cy^=main] button').first().click();
-
+    cy.selectBun();
+    cy.selectMain();
     cy.intercept(
       {
         method: 'POST',
@@ -42,9 +67,7 @@ describe('Burger constructor test', () => {
 
     cy.get('[data-cy=orderSummary] button').click();
     cy.get('[data-cy=modal]').should('contain.text', '1111');
-    cy.get('[data-cy=modal] button').click();
-    cy.get('[data-cy=noUpperBun]').should('have.text', 'Выберите булки');
-    cy.get('[data-cy=noIngredients]').should('have.text', 'Выберите начинку');
-    cy.get('[data-cy=noLowerBun]').should('have.text', 'Выберите булки');
+    cy.closeModal();
+    cy.isClearConstructor();
   });
 });
